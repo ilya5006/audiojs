5fde0e743dd171e1a5c731956fa8b6623cbb7360
ilya5006 <ilya.samarczew2011@yandex.ru>
2020-08-03 20:38:22 +0300

    Init AudioVisualizer on click "select song" button (bug-fix)

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,9 +5,22 @@ const canvas = document.querySelector('canvas');
 const songFileInput = document.querySelector('#songFileInput');
 const songTitle = document.querySelector('#song-title');
 
-const audioVisualizer = new AudioVisualizer({
-    'songTitle': songTitle,
-    'audio': audio,
-    'canvas': canvas,
-    'songFileInput': songFileInput
-});
+const initAudioVisualizer = () => {
+    const audioVisualizer = new AudioVisualizer({
+        'songTitle': songTitle,
+        'audio': audio,
+        'canvas': canvas,
+        'songFileInput': songFileInput
+    });
+
+    songFileInput.removeEventListener('click', initAudioVisualizer);
+}
+
+const audio = document.querySelector('audio');
+const canvas = document.querySelector('canvas');
+const songFileInput = document.querySelector('#songFileInput');
+const songTitle = document.querySelector('#song-title');
+
+songFileInput.addEventListener('click', initAudioVisualizer);
+
+
