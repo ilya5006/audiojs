e37d0c502143b6c3b3130a516fe359f943bb17aa
ilya5006 <ilya.samarczew2011@yandex.ru>
2020-08-25 21:25:11 +0300

    Some little changes

--- resources/js/classes/AudioVisualizer.js
+++ resources/js/classes/AudioVisualizer.js
@@ -4,7 +4,7 @@ export default class AudioVisualizer {
      * @param DOMElements {
      *    songTitle - element which will be show song title;
      *    audio - audio element which will be use for taking audiostream;
-     *    canvas - canvas element which contains the canvas and will be using for draw animaion;
+     *    canvas - canvas element which contains the canvas and will be using for draw animation;
      *    songFileInput - input (type=file) which using for getting user song;
      * }
      */
@@ -38,7 +38,7 @@ export default class AudioVisualizer {
     async updateSong(event) {
         const file = event.target.files[0];
 
-        if (!this.isAudioFile(file)) {
+        if (! this.isAudioFile(file)) {
             throw new Error('Загружаемый вами файл не является звуковым');
         }
 
@@ -82,13 +82,15 @@ export default class AudioVisualizer {
         let barHeight;
         let x = 0;
 
+        const MAX_HEIGHT = 255;
+
         for (let i = 0; i < bufferLength; i++) {
             barHeight = frequencyArr[i];
 
             this.canvasCtx.fillStyle = `hsl(
                 ${(360 / bufferLength * i) + this.colorShift},
                 ${100 / bufferLength * frequencyArr[i]}%,
-                ${100 / bufferLength * frequencyArr[i]}%
+                ${100 / MAX_HEIGHT * frequencyArr[i]}%
             )`;
 
             this.canvasCtx.fillRect(x, (this.HEIGHT - barHeight) / 2, barWidth, barHeight);
